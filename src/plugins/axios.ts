ce0d373922d025cdabbc36b5570987a639b652de
simonvennat <svennat@happy-garden.fr>
2026-01-07 01:47:35 +0100

    Updates domain name and configures Axios
    Updates the domain name in the .htaccess and robots.txt files
    to reflect the rebranding from viking-cup.fr to normandie-racing.fr.
    
    Refactors the Axios configuration into a plugin for API requests.
    This centralizes the base URL and prepares for future
    authentication handling.

--- src/plugins/axios.ts
+++ src/plugins/axios.ts
@@ -1,39 +1,7 @@
 import axios from 'axios'
-import { useAuthStore } from '@/stores/authStore'
 
 const api = axios.create({
   baseURL: import.meta.env.VITE_ADMIN_BASE_URL + '/api'
 })
-
-// api.interceptors.request.use(async (config) => {
-//   const authStore = useAuthStore()
-//
-//   const token = await authStore.getValidToken()
-//   if (token) {
-//     config.headers.Authorization = `Bearer ${token}`
-//   }
-//   return config
-// })
-//
-// api.interceptors.response.use(
-//   response => response,
-//   async error => {
-//     const originalRequest = error.config
-//     const authStore = useAuthStore()
-//
-//     if (error.response?.status === 401 && !originalRequest._retry) {
-//       originalRequest._retry = true
-//       try {
-//         const newToken = await authStore.refreshToken()
-//         originalRequest.headers.Authorization = `Bearer ${newToken}`
-//         return api(originalRequest)
-//       } catch (err) {
-//         console.error('Erreur lors du refresh token', err)
-//       }
-//     }
-//
-//     return Promise.reject(error)
-//   }
-// )
 
 export default api
